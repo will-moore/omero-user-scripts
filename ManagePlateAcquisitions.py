9e81d18f05089df5d9cbf24e3b01f6fe38a572e9
Nikola Klaric <nikola@klaric.org>
2013-07-17 20:31:33 +0200

    Refactored into single script. Changed Well updates to bulk save.

---
+++ ManagePlateAcquisitions.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+""" Add or remove PlateAcquisition(s) in a given Plate.
+"""
+from omero.util import script_utils
+from omero.gateway import BlitzGateway
+from omero.rtypes import *
+from omero.model import *
+import omero.scripts as scripts
+
+
+def run():
+    """
+    """
+    client = scripts.client("ManagePlateAcquisitions.py",
+        "Add or remove PlateAcquisition(s) in a given Plate",
+        scripts.List("IDs", optional=False, grouping="1",
+            description="List of Plate IDs",
+        ).ofType(rlong(0)),
+        scripts.String("Mode", optional=False, grouping="2",
+            description="Select if you want to add or remove PlateAcquisitions",
+            values=[rstring("Add"), rstring("Remove")], 
+            default="Add",
+        ),
+    )
+
+    try:
+        scriptParams = {}
+        for key in client.getInputKeys():
+            if client.getInput(key):
+                scriptParams[key] = client.getInput(key, unwrap=True)
+
+        connection = BlitzGateway(client_obj=client)
+        updateService = connection.getUpdateService()
+        queryService = connection.getQueryService()
+
+        processedMessages = []
+
+        for plateId in scriptParams["IDs"]:
+            plateObj = connection.getObject("Plate", plateId)
+            if plateObj is None:
+                client.setOutput("Message",
+                    rstring("ERROR: No Plate with ID %s" % plateId))
+                return
+
+            if scriptParams["Mode"] == "Add":
+                plateAcquisitionObj = PlateAcquisitionI()
+                plateAcquisitionObj.setPlate(PlateI(plateObj.getId(), False))
+
+                wellGrid = plateObj.getWellGrid()
+                for axis in wellGrid:
+                    for wellObj in axis:
+                        wellSampleList = wellObj.copyWellSamples()
+                        plateAcquisitionObj.addAllWellSampleSet(wellSampleList)
+
+                plateAcquisitionObj = updateService.saveAndReturnObject(
+                    plateAcquisitionObj)
+                plateAcquisitionId = plateAcquisitionObj.getId()._val
+
+                processedMessages.append(
+                    "Linked new PlateAcquisition with ID %d to Plate with ID %d."
+                        % (plateAcquisitionId, plateId))
+            else:
+                params = omero.sys.ParametersI()
+                params.addId(plateId)
+
+                queryString = """
+                    FROM PlateAcquisition AS pa
+                    LEFT JOIN FETCH pa.wellSample
+                    LEFT OUTER JOIN FETCH pa.annotationLinks
+                        WHERE pa.plate.id = :id
+                    """
+                plateAcquisitionList = queryService.findAllByQuery(
+                    queryString, params, connection.SERVICE_OPTS)
+                if plateAcquisitionList:
+                    updateList = []
+
+                    for plateAcquisitionObj in plateAcquisitionList:
+                        for wellSampleObj in plateAcquisitionObj.copyWellSample():
+                            wellSampleObj.setPlateAcquisition(None)
+                            updateList.append(wellSampleObj)
+
+                        updateService.saveArray(updateList)
+
+                        plateAcquisitionObj.clearWellSample()
+                        plateAcquisitionObj.clearAnnotationLinks()
+
+                        plateAcquisitionObj = updateService.saveAndReturnObject(
+                            plateAcquisitionObj)
+                        updateService.deleteObject(plateAcquisitionObj)
+
+                processedMessages.append(
+                    "%d PlateAcquisition(s) removed from Plate with ID %d."
+                        % (len(plateAcquisitionList), plateId))
+
+        client.setOutput("Message", rstring("No errors. %s"
+            % " ".join(processedMessages)))
+    finally:
+        client.closeSession()
+
+if __name__ == "__main__":
+    run()
