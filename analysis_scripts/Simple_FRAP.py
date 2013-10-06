59d88bab63319eba0dd353266dabe7c8844a7c67
Sebastien Besson <seb.besson@gmail.com>
2013-10-06 09:44:44 +0100

    Fix flake8 warning for Simple_FRAP.py

--- analysis_scripts/Simple_FRAP.py
+++ analysis_scripts/Simple_FRAP.py
@@ -22,16 +22,16 @@
 
 ------------------------------------------------------------------------------
 
-Simply analyses the average pixel intensity for ellipses in a 
+Simply analyses the average pixel intensity for ellipses in a
 movie and attempts to estimate the half-time of FRAP recovery
 and the percent recovery (mobile fraction).
-Needs ellipses drawn on all timepoints to be included in the 
+Needs ellipses drawn on all timepoints to be included in the
 analysis, and needs at least 1 pre-bleach frame.
 """
 
 import omero
 import omero.scripts as scripts
-from omero.rtypes import rdouble, rint, rlong, rstring
+from omero.rtypes import rint, rlong, rstring
 from omero.gateway import BlitzGateway
 import omero.util.script_utils as scriptUtil
 
@@ -39,8 +39,8 @@ import omero.util.script_utils as scriptUtil
 # To keep things simple, we'll work with a single Ellipse per T
 # =================================================================
 def getEllipses(conn, imageId):
-    """ 
-    Returns the a dict of tIndex: {'cx':cx, 'cy':cy, 'rx':rx, 'ry':ry, 'z':z} 
+    """
+    Returns the a dict of tIndex: {'cx':cx, 'cy':cy, 'rx':rx, 'ry':ry, 'z':z}
     NB: Assume only 1 ellipse per time point
 
     @param conn:    BlitzGateway connection
@@ -48,7 +48,8 @@ def getEllipses(conn, imageId):
     """
 
     ellipses = {}
-    result = conn.getRoiService().findByImage(imageId, None, conn.SERVICE_OPTS)
+    result = conn.getRoiService().findByImage(
+        imageId, None, conn.SERVICE_OPTS)
 
     for roi in result.rois:
         for shape in roi.copyShapes():
@@ -59,13 +60,13 @@ def getEllipses(conn, imageId):
                 ry = int(shape.getRy().getValue())
                 z = int(shape.getTheZ().getValue())
                 t = int(shape.getTheT().getValue())
-                ellipses[t] = {'cx':cx, 'cy':cy, 'rx':rx, 'ry':ry, 'z':z}
+                ellipses[t] = {'cx': cx, 'cy': cy, 'rx': rx, 'ry': ry, 'z': z}
     return ellipses
 
 
 def getEllipseData(image, ellipses, theC=0):
-    """ Returns a dict of t:averageIntensity for all ellipses. 
-    
+    """ Returns a dict of t:averageIntensity for all ellipses.
+
     @param ellipse:     The ellipse defined as a tuple (cx, cy, rx, ry, z, t)
     @returns:           A list of (x,y) points for the ellipse
     """
@@ -85,7 +86,9 @@ def getEllipseData(image, ellipses, theC=0):
         height = ry * 2
 
         # get pixel data for the 'tile'
-        tileData = image.getPrimaryPixels().getTile(theZ=e['z'], theC=theC, theT=t, tile=(xStart, yStart, width, height))
+        tileData = image.getPrimaryPixels().getTile(
+            theZ=e['z'], theC=theC, theT=t, tile=(xStart, yStart, width,
+                                                  height))
 
         # find the pixels within the ellipse
         pixelValues = []
@@ -104,9 +107,9 @@ def getEllipseData(image, ellipses, theC=0):
 
 def getTimes(conn, image, theC=0):
     """
-    Get a dict of tIndex:time (seconds) for the first plane (Z = 0) at 
+    Get a dict of tIndex:time (seconds) for the first plane (Z = 0) at
     each time-point for the defined image and Channel.
-    
+
     @param conn:        BlitzGateway connection
     @param image:       ImageWrapper
     @return:            A map of tIndex: timeInSecs
@@ -120,16 +123,16 @@ def getTimes(conn, image, theC=0):
     params.add("theZ", rint(0))
     params.add("pixelsId", rlong(pixelsId))
 
-    query = "from PlaneInfo as Info where Info.theZ=:theZ and Info.theC=:theC and pixels.id=:pixelsId"
+    query = "from PlaneInfo as Info where Info.theZ=:theZ and" \
+        " Info.theC=:theC and pixels.id=:pixelsId"
     infoList = queryService.findAllByQuery(query, params, conn.SERVICE_OPTS)
 
     timeMap = {}
     for info in infoList:
         tIndex = info.theT.getValue()
-        time = info.deltaT.getValue() 
+        time = info.deltaT.getValue()
         timeMap[tIndex] = time
-    return timeMap    
-    
+    return timeMap
 
 
 def analyseImage(conn, image, cIndex):
@@ -144,7 +147,6 @@ def analyseImage(conn, image, cIndex):
     # Get dictionary of tIndex:timeStamp (secs)
     timeValues = getTimes(conn, image)
 
-
     # We now have all the Data we need from OMERO
 
     # create lists of times (secs) and intensities...
@@ -154,8 +156,8 @@ def analyseImage(conn, image, cIndex):
     # ...Ordered by tIndex
     for t in range(image.getSizeT()):
         if t in intensityData:
-            timeList.append( timeValues[t] )
-            valueList.append( intensityData[t] )
+            timeList.append(timeValues[t])
+            valueList.append(intensityData[t])
 
     print "Analysing pixel values for %s time points" % len(timeList)
 
@@ -165,21 +167,26 @@ def analyseImage(conn, image, cIndex):
     bleachTime = timeList[bleachTindex]
     preBleachValue = valueList[bleachTindex-1]
 
-    print "Bleach at tIndex: %s, TimeStamp: %0.2f seconds" % (bleachTindex, bleachTime)
-    print "Before Bleach: %0.2f, After Bleach: %0.2f" % (preBleachValue, bleachValue)
+    print "Bleach at tIndex: %s, TimeStamp: %0.2f seconds" \
+        % (bleachTindex, bleachTime)
+    print "Before Bleach: %0.2f, After Bleach: %0.2f" \
+        % (preBleachValue, bleachValue)
 
     # Use last timepoint for max recovery
     recoveryValue = valueList[-1]
     endTimepoint = timeList[-1]
-    mobileFraction = (recoveryValue - bleachValue)/(preBleachValue - bleachValue)
+    mobileFraction = (recoveryValue - bleachValue) / \
+        (preBleachValue - bleachValue)
 
-    print "Recovered to: %0.2f, after %0.2f seconds" % (recoveryValue, endTimepoint)
+    print "Recovered to: %0.2f, after %0.2f seconds" \
+        % (recoveryValue, endTimepoint)
     print "Mobile Fraction: %0.2f" % mobileFraction
 
     halfRecovery = (recoveryValue + bleachValue)/2
 
     # quick & dirty - pick the first timepoint where we exceed half recovery
-    recoveryValues = valueList[bleachTindex:]   # just the values & times after bleach time
+    recoveryValues = valueList[bleachTindex:]
+    # just the values & times after bleach time
     recoveryTimes = timeList[bleachTindex:]
     for t, v in zip(recoveryTimes, recoveryValues):
         if v >= halfRecovery:
@@ -188,8 +195,7 @@ def analyseImage(conn, image, cIndex):
 
     print "tHalf: %0.2f seconds" % tHalf
 
-
-    csvLines = [ 
+    csvLines = [
         "Time (secs)," + ",".join([str(t) for t in timeList]),
         "\n",
         "Average pixel value," + ",".join([str(v) for v in valueList]),
@@ -227,29 +233,37 @@ def doFrapAnalysis(conn, scriptParams):
 
 def runAsScript():
     """
-    The main entry point of the script, as called by the client via the scripting service, passing the required parameters.
+    The main entry point of the script, as called by the client via the
+    scripting service, passing the required parameters.
     """
 
     dataTypes = [rstring('Image')]
 
-    client = scripts.client('Simple_FRAP.py', """Analyse average intensity within ellipses over time and
+    client = scripts.client(
+        'Simple_FRAP.py',
+        """Analyse average intensity within ellipses over time and \
 do simple FRAP analysis to get mobile fraction and half-time of recovery.
-Needs at least on pre-bleach timepoint and ellipses on all timepoints to be analysed.
+Needs at least on pre-bleach timepoint and ellipses on all timepoints to be \
+analysed.
 """,
 
-    scripts.String("Data_Type", optional=False, grouping="1",
-        description="Choose source of images (only Image supported)", values=dataTypes, default="Image"),
+        scripts.String(
+            "Data_Type", optional=False, grouping="1",
+            description="Choose source of images (only Image supported)",
+            values=dataTypes, default="Image"),
 
-    scripts.List("IDs", optional=False, grouping="2",
-        description="List of Image IDs to analyse.").ofType(rlong(0)),
+        scripts.List(
+            "IDs", optional=False, grouping="2",
+            description="List of Image IDs to analyse.").ofType(rlong(0)),
 
-    scripts.Int("Channel_Index", optional=False, grouping="3",
-        description="The channel to analyse.", default=1, min=1),
+        scripts.Int(
+            "Channel_Index", optional=False, grouping="3",
+            description="The channel to analyse.", default=1, min=1),
 
-    version = "4.4.8",
-    authors = ["William Moore", "OME Team"],
-    institutions = ["University of Dundee"],
-    contact = "ome-users@lists.openmicroscopy.org.uk",
+        version="4.4.8",
+        authors=["William Moore", "OME Team"],
+        institutions=["University of Dundee"],
+        contact="ome-users@lists.openmicroscopy.org.uk",
     )
 
     try:
@@ -273,7 +287,8 @@ Needs at least on pre-bleach timepoint and ellipses on all timepoints to be anal
             message = "No Images Analysed. See Info for more details"
         else:
             average = sum(results)/len(results)
-            message = "Average FRAP t-half (%s images): %0.2f seconds. " % (len(results), average)
+            message = "Average FRAP t-half (%s images): %0.2f seconds. " \
+                % (len(results), average)
 
         # Return the output - display Message:
         client.setOutput("Message", rstring(message))
