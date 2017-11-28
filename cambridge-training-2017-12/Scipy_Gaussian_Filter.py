dae50823e3dd04ae7727f7fd8333c00eb44258e5
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-11-28 12:08:54 +0000

    Clean up file

--- cambridge-training-2017-12/Scipy_Gaussian_Filter.py
+++ cambridge-training-2017-12/Scipy_Gaussian_Filter.py
@@ -60,10 +60,10 @@ def run(conn, params):
     """
     For each image defined in the script parameters run the correlation
     analyser and load the result into OMERO.
-    Returns the number of images processed or (-1) if there is a
-    parameter error).
-    @param conn:   The BlitzGateway connection
-    @param params: The script parameters
+    Returns the number of images processed or -1 if there is a
+    parameter error.
+    @param conn   The BlitzGateway connection
+    @param params The script parameters
     """
 
     print "Parameters = %s" % params
@@ -72,7 +72,7 @@ def run(conn, params):
         print "Please enter a valid window size for the Gaussian kernel"
         return -1
     window_size = params.get("Kernel_Window_Size")
-    
+
     if not params.get("Sigma"):
         print "Please enter a valid value for Sigma (>0)"
         return -1
@@ -102,55 +102,59 @@ def run(conn, params):
         for z in range(sizeZ):
             for c in range(sizeC):
                 for t in range(sizeT):
-                    zctList.append( (z,c,t) ) 
+                    zctList.append((z, c, t))
 
+        plane = planeGen(image, zctList, window_size, sigma)
+        name = image.getName() + "_numpy_image"
+        i = conn.createImageFromNumpySeq(plane, name, sizeZ, sizeC,
+                                         sizeT, description="Gaussian Filter",
+                                         dataset=dataset)
 
-        i = conn.createImageFromNumpySeq(planeGen(image, zctList, window_size, sigma), image.getName() + "_numpy_image", sizeZ, sizeC, sizeT, description="Gaussian Filter",dataset=dataset)
-        print 'Created new Image:%s Name:"%s"' % (i.getId(), i.getName())
-        imageId = i.getId()
-        image_ids.append(imageId)
-
-    print "LOG: Done printing %d images" % len(images)
+        image_ids.append(i.getId())
 
     return image_ids
+
 
 def planeGen(image, zctList, window, sigma):
     """generator will yield planes"""
 
     planes = image.getPrimaryPixels().getPlanes(zctList)
     for p in planes:
-        # data = rolling_ball_background(p, radius, light_background=True, smoothing=True)
-        data = spi.filters.gaussian_filter(p, sigma=sigma, truncate=(((window - 1)/2)-0.5)/sigma)
+        truncated_param = (((window - 1)/2)-0.5)/sigma
+        data = spi.filters.gaussian_filter(p, sigma=sigma,
+                                           truncate=truncated_param)
         yield data
 
-### OMERO Figure Methods
+# OMERO Figure Methods
 
 
 def get_panel_json(image, x, y, width, height, channel=None):
+    """
+    Export the results as OMERO.figure
+    """
 
     rv = imageMarshal(image)
 
     if channel is not None:
         for idx, ch in enumerate(rv['channels']):
             ch['active'] = idx == channel
-            # print ch['color']
             ch['color'] = 'ffffff'
 
     img_json = {
-        "labels":[],
+        "labels": [],
         "height": height,
         "channels": rv['channels'],
         "width": width,
         "sizeT": rv['size']['t'],
         "sizeZ": rv['size']['z'],
-        "dx":0,
-        "dy":0,
-        "rotation":0,
-        "imageId":image.getId(),
-        "name":"438CTR_01_5_R3D_D3D.dv",
+        "dx": 0,
+        "dy": 0,
+        "rotation": 0,
+        "imageId": image.getId(),
+        "name": image.getName(),
         "orig_width": rv['size']['width'],
-        "zoom":100,
-        "shapes":[],
+        "zoom": 100,
+        "shapes": [],
         "orig_height": rv['size']['height'],
         "theZ": rv['rdefs']['defaultZ'],
         "y": y,
@@ -167,35 +171,34 @@ def get_labels_json(panel_json, column, row):
     channels = panel_json['channels']
     imagename = panel_json['name']
     if row == 0:
-        labels.append({"text":channels[column]['label'],
-                       "size":4,
-                       "position":"leftvert",
-                       "color":"000000"
-                     })
+        labels.append({"text": channels[column]['label'],
+                       "size": 4,
+                       "position": "leftvert",
+                       "color": "000000"})
     if column == 0:
         labels.append({"text": imagename,
-                       "size":4,
-                       "position":"top",
-                       "color":"000000"
-                     })
+                       "size": 4,
+                       "position": "top",
+                       "color": "000000"})
     return labels
+
 
 def create_figure_file(conn, image_ids):
 
-    width = 512/10;
-    height = 512/10;
-    spacing_x = 512/50;
-    spacing_y = 512/50;
+    width = 512/10
+    height = 512/10
+    spacing_x = 512/50
+    spacing_y = 512/50
     page_width = (width + spacing_x) * (5) * 1.25
     page_height = (height + spacing_y) * (len(image_ids)) * 1.25
 
     JSON_FILEANN_NS = "omero.web.figure.json"
 
-    figure_json = {"version":2,
-                   "paper_width":page_width,
-                   "paper_height":page_height,
-                   "page_size":"mm",
-                   "figureName":"from script",
+    figure_json = {"version": 2,
+                   "paper_width": page_width,
+                   "paper_height": page_height,
+                   "page_size": "mm",
+                   "figureName": "from script",
                    }
 
     curr_x = 0
@@ -204,9 +207,12 @@ def create_figure_file(conn, image_ids):
     column_count = 2
     offset = 10
 
+    gid = -1
     for z, image_id in enumerate(image_ids):
         image = conn.getObject('Image', image_id)
         curr_x = z * (width + spacing_x) + offset
+        if z == 0:
+            gid = image.getDetails().getGroup().getId()
         for c in range(image.getSizeC()):
             curr_y = c * (height + spacing_y) + offset
             j = get_panel_json(image, curr_x, curr_y, width, height, c)
@@ -226,32 +232,28 @@ def create_figure_file(conn, image_ids):
     description['imageId'] = first_img_id
 
     # Try to set Group context to the same as first image
-    conn.SERVICE_OPTS.setOmeroGroup('-1')
-    i = conn.getObject("Image", first_img_id)
-    gid = i.getDetails().getGroup().getId()
     conn.SERVICE_OPTS.setOmeroGroup(gid)
 
     json_string = json.dumps(figure_json)
     file_size = len(json_string)
     f = StringIO()
-    # f.write(figure_json)
     json.dump(figure_json, f)
 
     update = conn.getUpdateService()
-    orig_file = create_original_file_from_file_obj(conn, 
-        f, '', figure_name, file_size, mimetype="application/json")
+    orig_file = create_original_file_from_file_obj(conn, f, '', figure_name,
+                                                   file_size,
+                                                   mimetype="application/json")
     fa = omero.model.FileAnnotationI()
     fa.setFile(omero.model.OriginalFileI(orig_file.getId(), False))
     fa.setNs(wrap(JSON_FILEANN_NS))
     desc = json.dumps(description)
     fa.setDescription(wrap(desc))
     fa = update.saveAndReturnObject(fa, conn.SERVICE_OPTS)
-    file_id = fa.getId().getValue()
-
-    print "Figure Created:", file_id
+    return fa.getId().getValue()
 
 
-def create_original_file_from_file_obj(conn, fo, path, name, file_size, mimetype=None):
+def create_original_file_from_file_obj(conn, fo, path, name, file_size,
+                                       mimetype=None):
     """
     Creates and saves an OMERO OriginalFile from a file object
     """
@@ -264,20 +266,7 @@ def create_original_file_from_file_obj(conn, fo, path, name, file_size, mimetype
     if mimetype:
         original_file.mimetype = wrap(mimetype)
     original_file.setSize(rlong(file_size))
-    # set sha1  # ONLY for OMERO-4
-    try:
-        import hashlib
-        hash_sha1 = hashlib.sha1
-    except:
-        import sha
-        hash_sha1 = sha.new
-    try:
-        fo.seek(0)
-        h = hash_sha1()
-        h.update(fo.read())
-        original_file.setSha1(wrap(h.hexdigest()))
-    except:
-        pass       # OMERO-5 doesn't need this
+
     upd = conn.getUpdateService()
     original_file = upd.saveAndReturnObject(original_file, conn.SERVICE_OPTS)
 
@@ -315,7 +304,7 @@ if __name__ == "__main__":
 
         scripts.List(
             "IDs", optional=False, grouping="2",
-            description="Project or Dataset or Image or Plate or Screen ID.").ofType(rlong(0)),
+            description="Dataset IDs.").ofType(rlong(0)),
 
         scripts.Int(
             "Kernel_Window_Size", optional=False, grouping="3", default=20,
@@ -328,7 +317,7 @@ if __name__ == "__main__":
         scripts.Bool(
             "Create_Omero_Figure", default=True, grouping="5",
             description="Create An OMERO.Figure from the resultant images"),
-        
+
         authors=["Balaji Ramalingam", "OME Team"],
         institutions=["University of Dundee"],
         contact="ome-users@lists.openmicroscopy.org.uk",
@@ -346,7 +335,7 @@ if __name__ == "__main__":
         conn = BlitzGateway(client_obj=client)
         # # Call the main script - returns the number of images processed
         image_ids = run(conn, scriptParams)
-        
+
         if scriptParams["Create_Omero_Figure"]:
             create_figure_file(conn, image_ids)
 
