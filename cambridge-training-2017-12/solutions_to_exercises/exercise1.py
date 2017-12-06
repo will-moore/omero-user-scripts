7550e49c20764b15d1b7041e7c7b01da8ae60302
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-12-06 13:48:23 +0000

    Add import

--- cambridge-training-2017-12/solutions_to_exercises/exercise1.py
+++ cambridge-training-2017-12/solutions_to_exercises/exercise1.py
@@ -1,3 +1,8 @@
+# Add the following import at the top of the files
+from omero.model import DatasetAnnotationLinkI
+from omero.model import DatasetI
+from omero.gateway.model import TagAnnotationData
+
 # Insert the followig lines after the comment Exercise 1 changes to be added here
 tag = TagAnnotationData("new tag 2")
 tag.setTagDescription("new tag 2")
